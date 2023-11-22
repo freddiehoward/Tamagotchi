e0ed63a9de858f17cdd6b9d4e2a218853c276f94
freddiehoward <howard.f@etoncollege.org.uk>
2023-11-22 21:33:24 +0000

    finsished the tamagotchi by finishing methods, adding a menu, and adding random events every cycle

--- Tamagotchi/main.swift
+++ Tamagotchi/main.swift
@@ -32,10 +32,12 @@ class Tamagotchi {
     
     func eat() {
         print("your tamagotchi ate an apple!")
+        print("")
         
         if self.hungerLevel == 10 {
             
             print("your tamagotchi is full")
+            print("")
             
         }
         
@@ -50,10 +52,12 @@ class Tamagotchi {
     func playWithTamagotchi() {
         
         print("your tamagotchi did a dance!")
+        print("")
         
         if self.happinessLevel == 10 {
             
             print("your tamagotchi is very happy already")
+            print("")
             
         }
         
@@ -67,22 +71,26 @@ class Tamagotchi {
     func sleep() {
         
         print("your tamagotchi slept")
+        print("")
         
         if self.energyLevel == 10 && self.healthLevel == 10{
             
             print("your tamagotchi is already well rested and healthy so couldn't sleep")
+            print("")
             
         }
         
         else if self.energyLevel == 10 && self.healthLevel != 10 {
             
             print("your tamagotchi couldn't sleep but healed up")
+            print("")
             self.healthLevel += 1
         }
         
         else if self.energyLevel != 10 && self.healthLevel == 10 {
             
             print("your tamagotchi slept but didn't heal as it was already healthy")
+            print("")
             
         }
         else {
@@ -99,6 +107,7 @@ class Tamagotchi {
     func trainTamagotchi() {
         
         print("you trained your tamagotchi!")
+        print("")
         
         if self.trainingLevel == 10 {
             
@@ -116,23 +125,139 @@ class Tamagotchi {
     
     //MENU FUNCTION
     
-    func showMenu() {
+    func showMenuAndActOffInput(){
         
         let menu = """
 1: feed tamagotchi
 2: play with tamagotchi
 3: put tamagotchi to sleep
 4: train tamagotchi
+
+please enter the corresponding number to your action here:
 """
         print(menu)
         
+        if let userInput = readLine(){
+            if let intUserInput = Int(userInput) {
+                if intUserInput < 0 || intUserInput > 4 {
+                    print("Invalid input, please stick to the range shown")
+                }
+                
+            }
+            
+            if userInput == "1" {
+                self.eat()
+            }
+            
+            if userInput == "2" {
+                self.playWithTamagotchi()
+            }
+            
+            if userInput == "3" {
+                self.sleep()
+            }
+            
+            if userInput == "4" {
+                self.trainTamagotchi()
+            }
+            
+        }
+        
+        print("hunger is \(self.hungerLevel)")
+        print("happiness is \(self.happinessLevel)")
+        print("energy is \(self.energyLevel)")
+        print("health is \(self.healthLevel)")
+        print("age is \(self.ageLevel)")
+        print("weight is \(self.weightLevel)")
+        print("training level is \(self.trainingLevel)")
+        
+        
+        
+        var randomNumber = Int.random(in: 1...4)
+        
+        if randomNumber == 1 {
+            print("tamagotchi got hungrier")
+            
+            if self.hungerLevel == 0 {
+                print("tamagotchi dead")
+            }
+            
+            else{
+                self.hungerLevel -= 1
+            }
+        }
+        
+        if randomNumber == 2 {
+            print("tamagotchi got more bored")
+            print("")
+            
+            if self.happinessLevel == 0 {
+                print("tamagotchi died of boredom")
+                print("")
+            }
+            
+            else{
+                self.happinessLevel -= 1
+            }
+        }
+        
+        if randomNumber == 3 {
+            print("tamagotchi got more tired")
+            print("")
+            
+            if self.energyLevel == 0 {
+                print("tamagotchi dead from exhaustion")
+                print("")
+            }
+            
+            else{
+                self.energyLevel -= 1
+            }
+        }
+        
+        if randomNumber == 4 {
+            print("tamagotchi lost discipline")
+            print("")
+            
+            if self.trainingLevel == 0 {
+                print("tamagotchi went feral")
+                print("")
+            }
+            
+            else{
+                self.trainingLevel -= 1
+            }
+        }
+        
+        self.ageLevel += 1
+        
+        print("do you wish to continue with caring for the tamagotchi (1 = yes, 0 = no): ")
+        print("")
+        
+        if let userCarryOnInput = readLine() {
+            if userCarryOnInput == "0" {
+                print("you abandoned the tamagotchi")
+                print("")
+            }
+            if userCarryOnInput == "1" {
+                self.showMenuAndActOffInput()
+            }
+            
+            else{
+                print("invalid input")
+                print("")
+            }
+        }
+        
     }
     
-// make func to get user input
+
     
 }
 
 var tamagotchi1 = Tamagotchi(hungerLevel: 9, happinessLevel: 8, energyLevel: 7, healthLevel: 9, ageLevel: 0, weightLevel: 4, trainingLevel: 0)
+
+print(tamagotchi1.showMenuAndActOffInput())
 
 
 
